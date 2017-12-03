69af8dcb5290e8caf0fded59536dcf39c31ad21b
masterung <suchotel@gmail.com>
2017-12-03 10:33:02 +0700

    Success Add Fragment

--- app/src/main/java/com/intentprochote/exchange/ouudexchange/MainActivity.java
+++ app/src/main/java/com/intentprochote/exchange/ouudexchange/MainActivity.java
@@ -3,11 +3,20 @@ package com.intentprochote.exchange.ouudexchange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.intentprochote.exchange.ouudexchange.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //Add Fragment to Activity
+        //ifcause boolean if (boolean) {True}
+        if (savedInstanceState == null){getSupportFragmentManager()
+        .beginTransaction()
+        .add(R.id.ContentMainFragment, new MainFragment())
+        .commit();}//if True
+
+    } //Main Method (onCreate ทำงานก่อนเสมอ)
+} //Main Class
